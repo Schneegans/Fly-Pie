0c100bdcdc5eb7d81ff971adad0f981a51fdf3fc
Simon Schneegans <code@simonschneegans.de>
2020-08-27 17:05:55 +0200

    :wrench: Unlock medals in tutorial

--- settings/Tutorial.js
+++ settings/Tutorial.js
@@ -13,6 +13,7 @@ const {Gio, GdkPixbuf} = imports.gi;
 const Me            = imports.misc.extensionUtils.getCurrentExtension();
 const utils         = Me.imports.common.utils;
 const DBusInterface = Me.imports.common.DBusInterface.DBusInterface;
+const Timer         = Me.imports.common.Timer.Timer;
 const ExampleMenu   = Me.imports.settings.ExampleMenu.ExampleMenu;
 
 const DBusWrapper = Gio.DBusProxy.makeProxyWrapper(DBusInterface.description);
@@ -32,10 +33,30 @@ var Tutorial = class Tutorial {
       this._builder  = builder;
       this._settings = settings;
 
+      this._timer = new Timer();
+
       // Connect to the server so that we can toggle menu previews from the menu editor.
       new DBusWrapper(
           Gio.DBus.session, 'org.gnome.Shell', '/org/gnome/shell/extensions/flypie',
-          proxy => this._dbus = proxy);
+          proxy => {
+            this._dbus = proxy;
+            this._dbus.connectSignal('OnSelect', (proxy, sender, [id, path]) => {
+              try {
+                if (id == this._lastID && path == '/1/2/0') {
+                  const time     = this._timer.getElapsed();
+                  const bestTime = this._settings.get_double('best-tutorial-time');
+
+                  this._settings.set_double('last-tutorial-time', time);
+
+                  if (time < bestTime) {
+                    this._settings.set_double('best-tutorial-time', time);
+                  }
+                }
+              } catch (error) {
+                utils.notification('Failed to setup tutorial page: ' + error);
+              }
+            });
+          });
 
       const tutorialPages = 6;
 
@@ -78,10 +99,27 @@ var Tutorial = class Tutorial {
       for (let i = 1; i <= buttons; i++) {
         this._builder.get_object('tutorial-button-' + i).connect('clicked', () => {
           if (this._dbus) {
-            this._dbus.ShowCustomMenuRemote(JSON.stringify(ExampleMenu.get()), () => {});
+            this._dbus.ShowCustomMenuRemote(JSON.stringify(ExampleMenu.get()), (id) => {
+              if (id >= 0) {
+                this._timer.reset();
+                this._lastID = id;
+              }
+            });
           }
         });
       }
+
+      this._settings.connect('changed::best-tutorial-time', () => this._updateState());
+      this._settings.connect('changed::last-tutorial-time', () => this._updateState());
+
+      for (let i = 1; i <= 2; i++) {
+        this._builder.get_object('tutorial-reset-button-' + i).connect('clicked', () => {
+          this._settings.reset('best-tutorial-time');
+          this._settings.reset('last-tutorial-time');
+        });
+      }
+
+      this._updateState();
 
     } catch (error) {
       utils.notification('Failed to setup tutorial page: ' + error);
@@ -89,4 +127,24 @@ var Tutorial = class Tutorial {
   }
 
   // ----------------------------------------------------------------------- private stuff
+
+  _updateState() {
+    const bestTime = this._settings.get_double('best-tutorial-time');
+    const lastTime = this._settings.get_double('last-tutorial-time');
+
+    let text = '<big>Last selection time: <b>%d ms</b></big>'.format(lastTime);
+    this._builder.get_object('last-selection-time-1').label = text;
+    this._builder.get_object('last-selection-time-2').label = text;
+
+    text = '<big>Best selection time: <b>%d ms</b></big>'.format(bestTime);
+    this._builder.get_object('best-selection-time-1').label = text;
+    this._builder.get_object('best-selection-time-2').label = text;
+
+    this._builder.get_object('gold-medal-1').opacity   = bestTime < 1000 ? 1.0 : 0.1;
+    this._builder.get_object('gold-medal-2').opacity   = bestTime < 500 ? 1.0 : 0.1;
+    this._builder.get_object('silver-medal-1').opacity = bestTime < 2000 ? 1.0 : 0.1;
+    this._builder.get_object('silver-medal-2').opacity = bestTime < 750 ? 1.0 : 0.1;
+    this._builder.get_object('bronze-medal-1').opacity = bestTime < 3000 ? 1.0 : 0.1;
+    this._builder.get_object('bronze-medal-2').opacity = bestTime < 1000 ? 1.0 : 0.1;
+  }
 }
