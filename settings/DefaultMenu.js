dd90d3f979843ad097bbe4f84d83a28cad25a88a
Simon Schneegans <code@simonschneegans.de>
2020-10-08 10:18:30 +0200

    :wrench: Use favorites in default menu

--- settings/DefaultMenu.js
+++ settings/DefaultMenu.js
@@ -19,8 +19,6 @@ var DefaultMenu = class DefaultMenu {
 
   // -------------------------------------------------------------------- public interface
 
-  // Most parts of the menu are hard-coded. Some applications however are chosen based on
-  // the user's defaults.
   static get() {
 
     const menu = {
@@ -127,120 +125,15 @@ var DefaultMenu = class DefaultMenu {
           type: 'Bookmarks',
         },
         {
-          name: 'Default Apps',
-          icon: 'emblem-favorite',
-          type: 'Submenu',
-          children: [
-            {
-              name: 'Fly-Pie Settings',
-              icon: 'preferences-system',
-              type: 'Command',
-              data: 'gnome-extensions prefs flypie@schneegans.github.com',
-            },
-            {
-              name: 'Internet',
-              icon: 'applications-internet',
-              type: 'Submenu',
-              children: []
-            },
-            {
-              name: 'Multimedia',
-              icon: 'applications-multimedia',
-              type: 'Submenu',
-              children: []
-            },
-            {
-              name: 'Utilities',
-              icon: 'applications-accessories',
-              type: 'Submenu',
-              children: [
-                {
-                  name: 'Terminal',
-                  icon: 'org.gnome.Terminal',
-                  type: 'Command',
-                  data: 'gnome-terminal',
-                },
-                {
-                  name: 'Files',
-                  icon: 'org.gnome.Nautilus',
-                  type: 'Command',
-                  data: 'nautilus --new-window %U',
-                },
-                {
-                  name: 'GNOME System Monitor',
-                  icon: 'org.gnome.SystemMonitor',
-                  type: 'Command',
-                  data: 'gnome-system-monitor',
-                }
-              ]
-            }
-          ]
-        }
+          name: 'Fly-Pie Settings',
+          icon: 'preferences-system',
+          type: 'Command',
+          data: 'gnome-extensions prefs flypie@schneegans.github.com',
+        },
+        {name: 'Favorites', icon: 'emblem-favorite', type: 'Favorites'}
       ]
     };
 
-    // Add some default applications. It's possible that the user has no such default
-    // applications, so we have to check this for each one...
-    const browser     = this._getForUri('http');
-    const mailClient  = this._getForUri('mailto');
-    const audioPlayer = this._getForMimeType('audio/ogg');
-    const videoPlayer = this._getForMimeType('video/ogg');
-    const imageViewer = this._getForMimeType('image/jpg');
-    const textEditor  = this._getForMimeType('text/plain');
-
-    if (mailClient != null) {
-      menu.children[3].children[1].children.push(mailClient);
-    }
-
-    if (browser != null) {
-      menu.children[3].children[1].children.push(browser);
-    }
-
-    if (audioPlayer != null) {
-      menu.children[3].children[2].children.push(audioPlayer);
-    }
-
-    if (videoPlayer != null) {
-      menu.children[3].children[2].children.push(videoPlayer);
-    }
-
-    if (imageViewer != null) {
-      menu.children[3].children[2].children.push(imageViewer);
-    }
-
-    if (textEditor != null) {
-      menu.children[3].children[3].children.push(textEditor);
-    }
-
     return menu;
-  }
-
-  // ----------------------------------------------------------------------- private stuff
-
-  // Returns an action configuration for an application which is default for the given
-  // mime type. For example, "text/plain" will result in an action for Gedit on many
-  // systems.
-  static _getForMimeType(mimeType) {
-    return this._getForAppInfo(Gio.AppInfo.get_default_for_type(mimeType, false));
-  }
-
-  // Returns an action configuration for an application which is default for the given uri
-  // scheme. For example, "http" will result in an action for Firefox on many systems.
-  static _getForUri(uri) {
-    return this._getForAppInfo(Gio.AppInfo.get_default_for_uri_scheme(uri));
-  }
-
-  // Returns an action configuration for the given Gio.AppInfo. Or null, if the given
-  // Gio.AppInfo is null.
-  static _getForAppInfo(info) {
-    if (info != null) {
-      return {
-        name: info.get_display_name(),
-        icon: info.get_icon().to_string(),
-        type: 'Command',
-        data: info.get_commandline()
-      };
-    }
-    return null;
   }
 }
