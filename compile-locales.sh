7cd5b7d8642cd74b58317872623a360f49fa85f9
daPhipz <30795174+daPhipz@users.noreply.github.com>
2020-11-16 16:23:59 +0100

    🔧 Add newline in compile-locales.sh
    Co-authored-by: Simon Schneegans <code@simonschneegans.de>

--- compile-locales.sh
+++ compile-locales.sh
@@ -13,7 +13,8 @@
 
 # Get the location of this script.
 FLYPIE="$( cd "$( dirname "$0" )" && pwd )"
-# Check if all necessary commands are available
+
+# Check if all necessary commands are available.
 if ! which msgfmt; then
   echo "ERROR: Could not find msgfmt. On Ubuntu based systems, check if the gettext package is installed!"
   exit 1
