bf46b9a03359beddc797de569edfed45edd2ad25
Philipp Kiemle <philipp.kiemle@gmail.com>
2020-11-22 16:02:13 +0100

    🔧 Add install option to 'create-release.sh'

--- create-release.sh
+++ create-release.sh
@@ -9,6 +9,7 @@
 # -------------------------------------------------------------------------------------- #
 
 # This script creates a new release of the Fly-Pie GNOME extension.
+# When the '-i' option is set, it installs it to the system.
 
 # Go to the location of this script.
 cd "$( cd "$( dirname "$0" )" && pwd )" || { echo "ERROR: Could not find the location of 'create-release.sh'."; exit 1; }
@@ -18,3 +19,18 @@ cd "$( cd "$( dirname "$0" )" && pwd )" || { echo "ERROR: Could not find the loc
 # Zip everything together
 zip -r flypie@schneegans.github.com.zip common daemon presets resources schemas settings locale && \
 zip -r flypie@schneegans.github.com.zip -- *.js metadata.json *.md LICENSE
+
+# Check whether the extension should be installed
+while getopts i FLAG; do
+	case $FLAG in
+		
+		i)  # shellcheck disable=2015
+        gnome-extensions install flypie@schneegans.github.com.zip --force && \
+        echo "Successfully installed the application! Now restart the Shell ('Alt'+'F2', then 'r')." || \
+        { echo "ERROR: Could not install the extension."; exit 1; }
+        rm flypie@schneegans.github.com.zip;;
+
+		*)	echo "Invalid flag! Use '-i' to install the extension to your system. To just build it, run the script without any flag."
+        exit 1;;
+	esac
+done
