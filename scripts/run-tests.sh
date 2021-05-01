d71f824a3c2d900d816d5a78b04458d3bbb72bcd
Simon Schneegans <code@simonschneegans.de>
2021-05-01 21:03:36 +0200

    [run-ci]

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -8,10 +8,11 @@
 #                                                                                        #
 # -------------------------------------------------------------------------------------- #
 
-# Exit the script when one command fails.
-set -e
+# This script executes several automated tests on Fly-Pie. To do this, Fly-Pie is
+# installed in a container running GNOME Shell on xvfb.
 
-# The version can be supplied as first parameter.
+# The GNOME Shell version can be supplied as first parameter. Currently 3.36, 3.38 and
+# 40.0 are supported.
 VERSION="${1:-3.38}"
 
 # TODO: Check valid version.
@@ -22,36 +23,53 @@ VERSION="${1:-3.38}"
 cd "$( cd "$( dirname "$0" )" && pwd )/.." || \
   { echo "ERROR: Could not find the repo root."; exit 1; }
 
-# Compile the extension.
+# First compile the extension.
 scripts/create-release.sh -s
 
 # Run the container in detached mode.
-podman pull ghcr.io/schneegans/gnome-shell-"$VERSION":latest
-POD_ID=$(podman run --rm -td ghcr.io/schneegans/gnome-shell-"$VERSION":latest)
+podman pull ghcr.io/schneegans/gnome-shell:"$VERSION"
+POD_ID=$(podman run --rm -td ghcr.io/schneegans/gnome-shell:"$VERSION")
 
 # Wait some time to make sure that GNOME Shell has been started.
-sleep 10
+echo -n "Booting the container..."
+sleep 3
+echo " Done."
 
-# Copy the archive to the container.
+# Copy the extension to the container.
+echo -n "Copy Fly-Pie to the container..."
 podman cp flypie@schneegans.github.com.zip "$POD_ID":/home/gnomeshell/
+echo " Done."
 
 # Execute the install script. This installs the extension, restarts GNOME Shell and
 # finally enables the extension.
+echo -n "Installing Fly-Pie..."
 podman exec --user gnomeshell "$POD_ID" /home/gnomeshell/set-env.sh /home/gnomeshell/enable-extension.sh flypie@schneegans.github.com
+sleep 1
+ERRORS=$(podman exec "$POD_ID" journalctl | grep flypie | grep ERROR)
 
-# Wait some time to make sure that GNOME Shell has been restarted.
-sleep 3
+if [ ! -z "$ERRORS" ]; then
+  echo " Failed!"
+  echo $ERRORS
+  exit 1
+else
+ echo " Done."
+fi
 
+
+echo -n "Opening the preferences dialog of Fly-Pie..."
 podman exec --user gnomeshell "$POD_ID" /home/gnomeshell/set-env.sh gnome-extensions prefs flypie@schneegans.github.com
 sleep 2
+ERRORS=$(podman exec "$POD_ID" journalctl | grep "Failed to open preferences")
 
-# podman exec --env DISPLAY=:99 --user gnomeshell "$POD_ID" xdotool key ctrl+space
-# sleep 1
-# podman exec --env DISPLAY=:99 --user gnomeshell "$POD_ID" xdotool key ctrl+space
-# sleep 1
-
-# Then make a "screenshot" and display the image.
-podman cp "$POD_ID":/home/gnomeshell/Xvfb_screen0 . && convert xwd:Xvfb_screen0 capture.jpg
+if [ ! -z "$ERRORS" ]; then
+  echo " Failed!"
+  echo $ERRORS
+  exit 1
+else
+ echo " Done."
+fi
 
 # Finally stop the container.
-podman stop "$POD_ID"
+echo -n "Stopping the container..."
+podman stop "$POD_ID" > /dev/null
+echo " Done."
