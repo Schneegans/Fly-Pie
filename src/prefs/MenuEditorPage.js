55399ee664c6402ce544bd2ed4b560dd28ae0c5b
Allan Nordhøy <epost@anotheragency.no>
2021-05-07 06:04:09 +0000

    Spelling: Keep menu hiearchies efficient

--- src/prefs/MenuEditorPage.js
+++ src/prefs/MenuEditorPage.js
@@ -978,7 +978,7 @@ var MenuEditorPage = class MenuEditorPage {
           .replace('%s', 'https://github.com/Schneegans/Fly-Pie/issues'),
       // Translators: This is one of the hints which are shown in the bottom right corner
       // of the menu editor.
-      _('Deep hierarchies are fairly inefficient. Avoid putting menus into menus in menus.'),
+      _('Keep menu hierarchies efficient by putting the right amount of menus into new ones.'),
       // Translators: This is one of the hints which are shown in the bottom right corner
       // of the menu editor.
       _('If you delete all menus, log out and log in again, the default configuration will be restored.'),
