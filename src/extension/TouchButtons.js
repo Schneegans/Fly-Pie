3acce2739e1cb48c64f3d40e7e007805aaac74e6
Simon Schneegans <code@simonschneegans.de>
2021-11-10 20:56:01 +0100

    :tada: Open menus on click

--- src/extension/TouchButtons.js
+++ src/extension/TouchButtons.js
@@ -8,38 +8,68 @@
 
 'use strict';
 
-const Main            = imports.ui.main;
-const {Meta, Clutter} = imports.gi;
+const Main                 = imports.ui.main;
+const {Meta, Clutter, Gio} = imports.gi;
 
-const Me       = imports.misc.extensionUtils.getCurrentExtension();
-const utils    = Me.imports.src.common.utils;
-const MenuItem = Me.imports.src.extension.MenuItem.MenuItem;
+const Me            = imports.misc.extensionUtils.getCurrentExtension();
+const utils         = Me.imports.src.common.utils;
+const DBusInterface = Me.imports.src.common.DBusInterface.DBusInterface;
+const MenuItem      = Me.imports.src.extension.MenuItem.MenuItem;
+
+const DBusWrapper = Gio.DBusProxy.makeProxyWrapper(DBusInterface.description);
 
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
+
+
+const INACTIVE_OPACITY = 50;
+const DRAG_OPACITY     = 100;
+const DRAG_SCALE       = 0.7;
 
 var TouchButtons = class TouchButtons {
 
   // ------------------------------------------------------------ constructor / destructor
 
   constructor(settings) {
+
     this._settings          = settings;
     this._touchButtons      = [];
     this._configs           = [];
     this._startupCompleteID = 0;
+    this._inOverview        = false;
+
+    // Connect to the server so that we can toggle the demo menu from the tutorial.
+    new DBusWrapper(
+        Gio.DBus.session, 'org.gnome.Shell', '/org/gnome/shell/extensions/flypie',
+        proxy => {
+          this._dbus = proxy;
+          this._dbus.connectSignal('OnSelect', () => this._toggleButtons(true));
+          this._dbus.connectSignal('OnCancel', () => this._toggleButtons(true));
+        });
+
+    this._shownOverviewID = Main.overview.connect('showing', () => {
+      this._toggleButtons(false);
+      this._inOverview = true;
+    });
+    this._hideOverviewID  = Main.overview.connect('hiding', () => {
+      this._toggleButtons(true);
+      this._inOverview = false;
+    });
 
     this.onSettingsChange();
   }
 
   destroy() {
     this._touchButtons.forEach(button => button.destroy());
-
     this._touchButtons = [];
 
     if (this._startupCompleteID) {
       Main.layoutManager.disconnect(this._startupCompleteID);
       this._startupCompleteID = 0;
     }
+
+    Main.overview.disconnect(this._shownOverviewID);
+    Main.overview.disconnect(this._hideOverviewID);
   }
 
   // -------------------------------------------------------------------- public interface
@@ -61,16 +91,15 @@ var TouchButtons = class TouchButtons {
 
     // clang-format off
     this._cachedSettings = {
-      globalScale:         globalScale,
-      easingDuration:      this._settings.get_double('easing-duration') * 1000,
-      textColor:           Clutter.Color.from_string(this._settings.get_string('text-color'))[1],
-      font:                this._settings.get_string('font'),
-      size:                this._settings.get_double('center-size-hover') * globalScale,
-      iconScale:           this._settings.get_double('center-icon-scale-hover'),
-      iconCrop:            this._settings.get_double('center-icon-crop-hover'),
-      iconOpacity:         this._settings.get_double('center-icon-opacity-hover'),
-      backgroundImage:     MenuItem.loadBackgroundImage(this._settings.get_string('center-background-image-hover'),
-                                                        this._settings.get_double('center-size-hover') * globalScale)
+      easingDuration:  this._settings.get_double('easing-duration') * 1000,
+      textColor:       Clutter.Color.from_string(this._settings.get_string('text-color'))[1],
+      font:            this._settings.get_string('font'),
+      size:            this._settings.get_double('center-size-hover') * globalScale,
+      iconScale:       this._settings.get_double('center-icon-scale-hover'),
+      iconCrop:        this._settings.get_double('center-icon-crop-hover'),
+      iconOpacity:     this._settings.get_double('center-icon-opacity-hover'),
+      backgroundImage: MenuItem.loadBackgroundImage(this._settings.get_string('center-background-image-hover'),
+                                                    this._settings.get_double('center-size-hover') * globalScale)
     };
     // clang-format on
 
@@ -108,8 +137,16 @@ var TouchButtons = class TouchButtons {
 
   // ----------------------------------------------------------------------- private stuff
 
+  _toggleButtons(enable) {
+    this._touchButtons.forEach(button => {
+      button.reactive = enable;
+      this._ease(button, {opacity: enable ? INACTIVE_OPACITY : 0});
+    });
+  }
+
   _createButtons() {
-    this.destroy();
+    this._touchButtons.forEach(button => button.destroy());
+    this._touchButtons = [];
 
     this._configs.forEach(config => {
       if (config.touchButton) {
@@ -121,18 +158,22 @@ var TouchButtons = class TouchButtons {
         actor.name     = `Fly-Pie TouchButton (${config.name})`;
         actor.width    = this._cachedSettings.size;
         actor.height   = this._cachedSettings.size;
-        actor.opacity  = 20;
-        actor.reactive = true;
+        actor.opacity  = this._inOverview ? 0 : INACTIVE_OPACITY;
+        actor.reactive = !this._inOverview;
 
         Main.layoutManager.addChrome(actor);
         this._touchButtons.push(actor);
 
-        actor.connect('enter-event', () => {
-          this._ease(actor, {opacity: 255});
+        actor.connect('enter-event', (a) => {
+          if (!a._dragging) {
+            this._ease(actor, {opacity: 255});
+          }
         });
 
-        actor.connect('leave-event', () => {
-          this._ease(actor, {opacity: 20});
+        actor.connect('leave-event', (a) => {
+          if (!a._dragging) {
+            this._ease(actor, {opacity: INACTIVE_OPACITY});
+          }
         });
 
         actor.connect('motion-event', (a, event) => {
@@ -159,7 +200,6 @@ var TouchButtons = class TouchButtons {
             this._ease(actor, {opacity: 255});
             this._ease(actor, {scale_x: 1});
             this._ease(actor, {scale_y: 1});
-            utils.debug('drop');
           }
         });
 
@@ -172,9 +212,9 @@ var TouchButtons = class TouchButtons {
             [x, y]     = action.get_coords();
             [ok, x, y] = actor.transform_stage_point(x, y);
             actor.set_pivot_point(x / actor.width, y / actor.height);
-            this._ease(actor, {opacity: 50});
-            this._ease(actor, {scale_x: 0.5});
-            this._ease(actor, {scale_y: 0.5});
+            this._ease(actor, {opacity: DRAG_OPACITY});
+            this._ease(actor, {scale_x: DRAG_SCALE});
+            this._ease(actor, {scale_y: DRAG_SCALE});
 
             actor._dragging   = true;
             actor._dragStartX = x;
@@ -190,7 +230,9 @@ var TouchButtons = class TouchButtons {
 
 
           } else if (state == Clutter.LongPressState.CANCEL) {
-            utils.debug('show!');
+            this._toggleButtons(false);
+            this._dbus.ShowMenuAtRemote(
+                config.name, actor.x + actor.width / 2, actor.y + actor.height / 2);
           }
         });
         actor.add_action(action);
