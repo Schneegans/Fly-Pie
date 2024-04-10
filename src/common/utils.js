322a55b0c46aff8f83c27d2e8dd3b5cba7dadb40
Simon Schneegans <code@simonschneegans.de>
2024-04-10 20:53:09 +0200

    :beetle: Fix notifications on GNOME 46

--- src/common/utils.js
+++ src/common/utils.js
@@ -25,6 +25,50 @@ import PangoCairo from 'gi://PangoCairo';
 const St  = await importInShellOnly('gi://St');
 const Gtk = await importInPrefsOnly('gi://Gtk');
 
+// We import the Config module. This is done differently in the GNOME Shell process and in
+// the preferences process.
+const Config = await importConfig();
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Two methods for checking the current version of GNOME Shell.                         //
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// Returns the given argument, except for "alpha", "beta", and "rc". In these cases -3,
+// -2, and -1 are returned respectively.
+function toNumericVersion(x) {
+  switch (x) {
+    case 'alpha':
+      return -3;
+    case 'beta':
+      return -2;
+    case 'rc':
+      return -1;
+  }
+  return x;
+}
+
+const [GS_MAJOR, GS_MINOR] = Config.PACKAGE_VERSION.split('.').map(toNumericVersion);
+
+// This method returns true if the current GNOME Shell version matches the given
+// arguments.
+export function shellVersionIs(major, minor) {
+  return GS_MAJOR == major && GS_MINOR == toNumericVersion(minor);
+}
+
+// This method returns true if the current GNOME Shell version is at least as high as the
+// given arguments. Supports "alpha" and "beta" for the minor version number.
+export function shellVersionIsAtLeast(major, minor = 0) {
+  if (GS_MAJOR > major) {
+    return true;
+  }
+
+  if (GS_MAJOR == major) {
+    return GS_MINOR >= toNumericVersion(minor);
+  }
+
+  return false;
+}
+
 //////////////////////////////////////////////////////////////////////////////////////////
 // This method can be used to write a message to GNOME Shell's log. This is enhances    //
 // the standard log() functionality by prepending the extension's name and the location //
@@ -83,6 +127,17 @@ export async function importGettext() {
         .gettext;
   }
   return (await import('resource:///org/gnome/shell/extensions/extension.js')).gettext;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// This method can be used to import the Config module.                                 //
+//////////////////////////////////////////////////////////////////////////////////////////
+
+export async function importConfig() {
+  if (typeof global === 'undefined') {
+    return (await import('resource:///org/gnome/Shell/Extensions/js/misc/config.js'));
+  }
+  return (await import('resource:///org/gnome/shell/misc/config.js'));
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
